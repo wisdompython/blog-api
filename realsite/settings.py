de6f8d97f2d3f8d7944bc7764ec4f38e5406750f
Wisdom <69306560+wisdompython@users.noreply.github.com>
2024-10-24 12:38:49 +0100

    Update settings.py

--- realsite/settings.py
+++ realsite/settings.py
@@ -86,21 +86,7 @@ WSGI_APPLICATION = 'realsite.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'blog_db',
-#         'USER': 'dbmasteruser',
-#         'PASSWORD': 'byteWAVE_db_test',
-#         'HOST': 'ls-9df16d0519a20f8af7a9fd005a62dcddbf36765d.c6eqx9kp8zf0.eu-west-3.rds.amazonaws.com',
-#         'PORT': '3306',
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-#         },
 
-#     }
-
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
